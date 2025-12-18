144d7545095c40622762100550f5ce1ecf8b9516
Minji Kim <sml07113@gmail.com>
2025-12-18 16:58:33 +0900

    fix(front): 버튼 색깔 안보이는 문제 해결

--- frontend/src/global/component/Button.tsx
+++ frontend/src/global/component/Button.tsx
@@ -1,12 +1,20 @@
+type ButtonColor = 'red' | 'blue' | 'gray';
+
 export default function Button({ color, content, handler }: {
-  color: string;
+  color: ButtonColor;
   content: string;
   handler?: () => void;
 }) {
+  const colorClass = {
+    red: 'bg-red-500 hover:bg-red-400',
+    blue: 'bg-blue-500 hover:bg-blue-400',
+    gray: 'bg-gray-500 hover:bg-gray-400',
+  }[color];
+
   return (
     <button
       onClick={handler}
-      className={`bg-${color}-500 hover:bg-${color}-400 cursor-pointer text-white font-medium w-[120px] px-4 py-1 rounded-md`}
+      className={`${colorClass} cursor-pointer text-white font-medium w-[120px] px-4 py-1 rounded-md`}
     >
       {content}
     </button>
