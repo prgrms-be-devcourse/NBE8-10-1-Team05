d1ff01399b53373fb553b8f492a4a0764db1f3fe
Minji Kim <sml07113@gmail.com>
2025-12-19 15:50:59 +0900

    feat(front): 로그아웃 누르면 localStorage에 저장된 이메일 삭제

--- frontend/src/global/component/Header.tsx
+++ frontend/src/global/component/Header.tsx
@@ -1,8 +1,22 @@
 'use client';
 
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 export default function Header() {
+  const router = useRouter();
+
+  const handleLogout = () => {
+    if (!confirm('브라우저에 저장된 이메일을 지우고 초기 화면으로 돌아가시겠어요?')) {
+      return;
+    }
+    // 로컬 스토리지에 저장된 이메일 삭제
+    localStorage.removeItem('email');
+
+    // 초기 화면으로 이동
+    router.push('/');
+  }
+
   return (
     <header className="w-full bg-gray-800 text-white py-3 fixed">
       <div className="flex items-center justify-between px-5">
@@ -13,9 +27,9 @@ export default function Header() {
           <Link href="/order/listByEmail" className="hover:text-gray-300 cursor-pointer">
             나의 주문 내역
           </Link>
-          <Link href="/" className="hover:text-gray-300 cursor-pointer">
+          <div onClick={handleLogout} className="hover:text-gray-300 cursor-pointer">
             로그아웃
-          </Link>
+          </div>
         </div>
       </div>
     </header>
