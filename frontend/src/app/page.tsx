c9489e028a958c00fa98a53c0919f904c7b62322
Minji Kim <sml07113@gmail.com>
2025-12-19 15:40:47 +0900

    feat(front): 접속 페이지 구현, 이메일 localStorage 저장

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,13 +1,49 @@
 // 클라이언트 컴포넌트로 설정
 "use client";
 
-import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 export default function Home() {
+  const router = useRouter();
+  const [email, setEmail] = useState('');
+
+  const handleLogin = (email: string) => {
+    // 이메일 유효성 검사
+    if (!email || email.trim() === '') {
+      alert('이메일을 입력해주세요.');
+      return;
+    }
+
+    if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) {
+      alert('올바른 이메일 형식으로 입력해주세요.');
+      return;
+    }
+
+    // 로컬 스토리지 저장
+    localStorage.setItem('email', email);
+
+    // 주문 페이지로 이동
+    router.push('/order');
+  }
+
   return (
-    <>
-      <div>Grids & Circle</div>
-      <Link href="/order">주문 페이지 바로가기</Link>
-    </>
+    <div className="p-6 flex flex-col flex-1 justify-center items-center">
+      <div className="mb-6 font-extrabold text-5xl">Grids & Circle</div>
+      <div className="mb-10 font-medium text-2xl">이메일로 접속하기</div>
+      <input
+        type="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        className="text-black w-[60%] px-4 py-2 mb-4 border border-gray-300 rounded-lg bg-white"
+        placeholder="이메일을 입력하세요"
+      />
+      <button
+        onClick={() => handleLogin(email)}
+        className="w-[60%] bg-black text-white py-4 rounded-lg font-bold hover:bg-gray-800 cursor-pointer"
+      >
+        접속
+      </button>
+    </div>
   );
 }
