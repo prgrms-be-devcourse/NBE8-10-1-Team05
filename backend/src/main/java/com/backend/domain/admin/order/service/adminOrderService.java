467b312944a225066c57b443715d01882084854b
Minji Kim <sml07113@gmail.com>
2025-12-20 20:24:14 +0900

    feat: 각 주문 별 상품 합산하는 메소드 구현

--- backend/src/main/java/com/backend/domain/admin/order/service/adminOrderService.java
+++ backend/src/main/java/com/backend/domain/admin/order/service/adminOrderService.java
@@ -1,6 +1,9 @@
 package com.backend.domain.admin.order.service;
 
+import com.backend.domain.admin.order.dto.CombinedOrderItemDetail;
 import com.backend.domain.admin.order.dto.combinedOrderDto;
+import com.backend.domain.item.item.entity.Item;
+import com.backend.domain.item.item.service.ItemService;
 import com.backend.domain.order.order.entity.Order;
 import com.backend.domain.order.order.repository.OrderRepository;
 import com.backend.domain.order.orderItem.entity.OrderItem;
@@ -12,14 +15,18 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.format.DateTimeParseException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class adminOrderService {
+    private final ItemService itemService;
     private final OrderRepository orderRepository;
+
     @Transactional(readOnly = true)
     public List<combinedOrderDto> getOrdersByDueDate(String dueDateString) {
         LocalDateTime dueDate = parseDueDate(dueDateString);
@@ -46,10 +53,35 @@ public class adminOrderService {
         String address = orders.get(0).getAddress(); //주소
         String zipCode = orders.get(0).getZipCode(); // 우편번호
 
-        List<OrderItem> combinedOrderItems = orders.stream()
+        // 각 주문의 orderItems를 하나의 리스트로 넣기
+        List<OrderItem> orderItems = orders.stream()
                 .flatMap(order -> order.getOrderItems().stream())
-                .collect(Collectors.toList()); // OrderItem정보 찾기
+                .toList();
 
-        return new combinedOrderDto(email, address, zipCode, combinedOrderItems);
+        // 상품 개수를 합산하기
+        List<CombinedOrderItemDetail> combinedOrderItemDetails = getCombinedOrderItemDetail(orderItems);
+
+        return new combinedOrderDto(email, address, zipCode, combinedOrderItemDetails);
+    }
+
+    private List<CombinedOrderItemDetail> getCombinedOrderItemDetail(List<OrderItem> orderItems) {
+        List<CombinedOrderItemDetail> list = new ArrayList<>();
+
+        for (OrderItem oi : orderItems) {
+            Item i = itemService.findById(oi.getItemId()).orElse(null);
+            String itemName = (i == null) ? "삭제된 상품" : i.getName();
+
+            // 이미 list에 있는 상품이면 개수 합산
+            Optional<CombinedOrderItemDetail> found = list.stream()
+                    .filter(item -> item.getId() == oi.getItemId())
+                    .findFirst();
+            if (found.isPresent()) {
+                found.get().addQuantity(oi.getQuantity());
+            } else {
+                // list에 없는 상품이면 삽입
+                list.add(new CombinedOrderItemDetail(oi.getItemId(), itemName, oi.getQuantity()));
+            }
+        }
+        return list;
     }
 }
